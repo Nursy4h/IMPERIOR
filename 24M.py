e7ba355da571a324dfeafdc4a4fbbd151ab0f6de
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-07-23 21:56:32 +0700

    Create 24M.py

---
+++ 24M.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import time
+import random
+import cookielib
+import mechanize
+
+wd = "\033[90;1m" # dark
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+
+def runntxt(s):
+        for noobs in s + '\n':
+                sys.stdout.write(noobs)
+                sys.stdout.flush()
+                time.sleep(10. / 2100)
+               
+import os
+
+("clear")
+
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+
+import random
+import sys
+import time
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.2 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik(YY+'              .---.        .-----------N  ') 
+mengetik('             /     \  __  /    ------U  ')
+mengetik('            / /     \(  )/    -----R  ')
+mengetik('           //////     \/ `   ---S ')
+mengetik('          //// / // :    : ---Y  ')
+mengetik('         // /   /  /`     --4  ')
+mengetik('        //          //..\\\H  ')
+mengetik('               ====UU====UU==== 	')
+mengetik('               |    //||\\`    |    	 ')
+mengetik('               |      ```     |        ')
+mengetik('. N========A=======Z=======R=======I====L')
+mengetik('')
+mengetik(B+'	         INTRO 			')
+mengetik('')
+mengetik(RR+'.          hallo teman-teman.')
+mengetik('.        Nama saya M.NUR')
+mengetik('.          Agama saya islam    ')
+mengetik('.        Asal Saya Dari Sumut')
+mengetik('.         Saya Masih Skolah')
+mengetik('.        kegiatan saya hari hari')
+mengetik('.    Membantu Pekerjaan Org tua saya')
+mengetik('.  Ngegame(katanya sih nogame nolife) ')
+mengetik('. Game yg sya sukai pb.ff.oceanar..  ')
+mengetik('.                                                     ')
+mengetik('.     Status .........................         ')
+mengetik(GG+'. Pacaran sejak 2016 september 26 zampai sekang         ')
+mengetik('.          Kalo kalian pasti jomblo kan:-D             ')
+mengetik('.     pasti kalian bingung........        ')
+mengetik('. kenapa gw intro wkwkwkwkwkkwkwk   ')
+mengetik('.              tapi sih gw ga suka orang lain makek tanpa izin    ')
+mengetik('.                                jadi kalo ingin makek hack fb chat gw dari wa yah')
+mengetik('.          sewaktu waktu tools ini saya upgrade         ')
+mengetik('.  3.aturan      ')
+mengetik('.          1.jangan di sebar ke teman kalian      ')
+mengetik('.          2.jangan menyombongkan diri     ')
+mengetik('.          3.gunakan dengan bijak  ^-^    ')
+mengetik('. jika ketahuan kalian melanggar aturan saya hehehe.....  ')
+mengetik('.  tau sendiri lah yah ^_^')
+mengetik('.           dengan ini saya akhirin')
+mengetik('.     dengan ucapan ass@lamu@laikum wrb........      ')
+mengetik(B+'. ')
+mengetik('chat via whatshapp:0878-8114-5568')
+mengetik('aturan yang terpenting')
