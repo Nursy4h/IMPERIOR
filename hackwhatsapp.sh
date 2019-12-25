d1a0586352d7c19b95f0b034da84048b78a80d66
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-12-25 15:15:50 +0700

    Add files via upload

---
+++ hackwhatsapp.sh
@@ -0,0 +1,32 @@
+#!/system/xbin/bash
+#ZidanZet Gans
+#./ZET404
+echo "\033[31;1m [-]======================================[-]"
+echo "\033[32;1m [-]Coder    : ./ZET404 - ZidanZet Ganz   [-]" 
+echo "\033[31;1m [-]======================================[-]"
+echo "";
+read -p "Masukkan Nomer Lu (08xxx)=> " nomer;
+read -p "Masukkan Nomer Target (08xxxx) => " target;
+sleep 2
+echo "\033[32;1m [+] Scaning Target...";
+sleep 1
+echo "Login  => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "\033[32;1m [+] Exploiting Target...";
+echo "\033[32;1m [+] Done.";
+echo "Proses sedikit agak lama, tunggu beberapa waktu...";
+read -p "Verify Code => " code;
+sleep 2
+echo "\033[32;1m [+] Connecting to whatsapp.com...";
+echo "Login => $nomer";
+sleep 0.5
+echo "Code => $code";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "\033[32;1m [+] Sending Exploit...";
+sleep 5
+echo "\033[32;1m [+] Success Exploited...";
+exit;
